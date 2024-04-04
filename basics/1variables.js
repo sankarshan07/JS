96e04c22ad6ca0d03c08bed343950a0a5043dd0a
sankarshan07 <sankarshandesai3107@gmail.com>
2024-04-04 22:05:10 +0530

    added files

---
+++ basics/1variables.js
@@ -0,0 +1,10 @@
+const accountId =12354   //cannot be changed
+let accountEmail ="sankarshandesai3100@gmail.com"
+var accountPassword ="12345"
+accountCity = "banglore"
+
+// accountId = 2
+console.log(accountId);
+accountEmail =  'sankars96@gmail.com'
+console.log(accountEmail);
+console.table([accountEmail,accountId,accountCity])

3facb600580a20b3a838ab1b31352f0ddbd3096b
sankarshan07 <sankarshandesai3107@gmail.com>
2024-04-04 22:11:57 +0530

    modified

--- basics/1variables.js
+++ basics/1variables.js
@@ -3,8 +3,14 @@ let accountEmail ="sankarshandesai3100@gmail.com"
 var accountPassword ="12345"
 accountCity = "banglore"
 
-// accountId = 2
+let accountState ;  //variable is undefined
+
+/*
+prefer not to use var 
+beacuse of issure in block scope and fucntional scope
+*/
+
 console.log(accountId);
 accountEmail =  'sankars96@gmail.com'
 console.log(accountEmail);
-console.table([accountEmail,accountId,accountCity])
+console.table([accountEmail,accountId,accountCity,accountPassword,accountState])

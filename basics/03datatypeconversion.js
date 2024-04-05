d96568dc564a52788fe17b8eb740153f66d7fb3b
sankarshan07 <sankarshandesai3107@gmail.com>
2024-04-05 15:02:51 +0530

    changed

--- basics/03datatypeconversion.js
+++ basics/03datatypeconversion.js
@@ -1,34 +1,35 @@
 //datatype conversion
-let score = '25'
-console.log(typeof score)//output: string
+// let score = '25'
+// console.log(typeof score)//output: string
 
-let valueInNumber =Number(score)  //converting  the variable score to number data type using Number() method N must be capital
-console.log(typeof valueInNumber); 
+// let valueInNumber =Number(score)  //converting  the variable score to number data type using Number() method N must be capital
+// console.log(typeof valueInNumber); 
 
 //NaN- Not a number
-let a = true
-let b= false
-console.log(typeof a);
-console.log(typeof b);
-let valueInNumberA =Number(a)
-console.log(valueInNumberA);
-let valueInNumberB =Number(b)
-console.log(valueInNumberB);
+// let a = true
+// let b= false
+// console.log(typeof a);
+// console.log(typeof b);
+// let valueInNumberA =Number(a)
+// console.log(valueInNumberA);
+// let valueInNumberB =Number(b)
+// console.log(valueInNumberB);
 
-let n = "sanky"
-let nconvert = Number(n)
-console.log(nconvert);   //NaN -not a number 
+// let n = "sanky"
+// let nconvert = Number(n)
+// console.log(nconvert);   //NaN -not a number 
 
 // "" is false  "name " is true
 
 
 //number to boolean and booleam to number
-let isloggedIn = 1
-let loggedinB = Boolean(isloggedIn)  //Boolean B must be capital
-console.log(loggedinB);
+// let isloggedIn = 1
+// let loggedinB = Boolean(isloggedIn)  //Boolean B must be capital
+// console.log(loggedinB);
 
 
 // ****************operations*********************
 
 let value = 3
-console.log();
+let negvalue = -value
+console.log(negvalue);

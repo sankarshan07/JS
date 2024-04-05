72e4f90cd2df7ad78f4a0ea5f122e5321340f95e
sankarshan07 <sankarshandesai3107@gmail.com>
2024-04-05 21:53:31 +0530

    added summary datatype

---
+++ basics/04comparision.js
@@ -0,0 +1,10 @@
+console.log(2>1);
+console.log("2">1);    //typescript doesnt allow to compare different datatypes
+console.log(null >= 0);
+
+//equality check 
+
+
+//strict check ===
+// it not only checks the variable it also checks the datatype of the  varibale
+console.log("2"=== 2);

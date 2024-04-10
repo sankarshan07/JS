06c21432603640d2bfff6de130739b039b7ca976
sankarshan07 <sankarshandesai3107@gmail.com>
2024-04-10 21:46:32 +0530

    basics array 2

---
+++ basics 02/02_array.js
@@ -0,0 +1,5 @@
+const MarvelHeros =["thor","ironman", "spiderman"];
+const DcHeros =["superman", "batman", "wonder woman"]
+MarvelHeros.push(DcHeros)
+console.log(MarvelHeros);
+//if we push array into another array it doesnt merge the array rather it would add array within the array

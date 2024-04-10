53640e03c1ea47a3febb930fc0720b70ad7738fe
sankarshan07 <sankarshandesai3107@gmail.com>
2024-04-10 15:03:04 +0530

    datetime

--- basics/07dateinjs.js
+++ basics/07dateinjs.js
@@ -7,3 +7,13 @@ console.log(mydate.getTime());
 console.log(Date());
 
 console.log(mydate.toJSON());
+console.log(typeof mydate);
+
+let mycreateddate =new Date(2023,0,2)   //month starts with 0  for January 
+console.log(mycreateddate.toString());
+//time stamp 
+let mytimestamp =Date()
+console.log(Math.floor(Date.now()/1000));
+
+//string interpolation
+console.log(`The current year is ${mytimestamp.toString()}`);

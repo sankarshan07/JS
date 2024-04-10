e4fe1154fd54a0638b618385fb27c8f20d0ef564
sankarshan07 <sankarshandesai3107@gmail.com>
2024-04-11 00:13:21 +0530

    intial commit object

---
+++ basics 02/03objects.js
@@ -0,0 +1,18 @@
+// OBJECTS
+// objects can be created by 2 types one is by constructor and the other as literal 
+// singleton -constuctor
+// literal -multiple instance 
+
+
+//literals
+const user ={
+    name : "John",
+    age:30,
+    isloggedin :false,
+    location :"banglore"
+
+}
+//accesing  properties of object using dot notation
+console.log(user.name);
+//accessing  property using bracket notation
+console.log(user["age"]);

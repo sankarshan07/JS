fb678a4442a4c93f7a13e7258a23e14393c2849d
sankarshan07 <sankarshandesai3107@gmail.com>
2024-04-11 21:04:08 +0530

    added

--- basics 02/04_objects2.js
+++ basics 02/04_objects2.js
@@ -31,3 +31,37 @@ const obj2 = {
 const obj3 = {...obj1,...obj2}   //spread operator used to combine 
 
 console.log(obj3);
+
+const target = {
+    a:1, b:2 , c:3
+}
+
+const source ={
+    d:4, e:5, f:6
+}
+const source3 ={
+    d:42, e:57, f:61
+}
+//object.assign()  is used to merge two  objects into one object
+const newobj = Object.assign(target,source)
+console.log(newobj);
+console.log(target); 
+
+// if we have more thn 2 sources to be combined thn we use 
+const newobj1 = Object.assign({},target,source,source3)
+
+// when values come from database
+// array of users (here its is collectioln of different objects)
+const users =[
+    {id:1, name:"John"},
+    {id:2, name:"Sara"},
+
+]
+// accessing
+console.log(users[0].name);
+
+
+// to acces the keys and values from the objects
+console.log(Object.keys(target)); //will be returned in array
+console.log(Object.values(target));  //wil be returned in array
+console.log(Object.entries(target)) ; // will return an array of arrays [key,value]

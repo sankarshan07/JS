bbb57f8a4225cec68f8e4b25d0e8a7745f7acfee
sankarshan07 <sankarshandesai3107@gmail.com>
2024-04-12 18:54:54 +0530

    init scope

---
+++ 03_basics/02_Scopes.js
@@ -0,0 +1,21 @@
+// let var const 
+let a = 10 // local scope
+const b = 20
+var  c = 30  // global scope -through out the programm
+
+if (true){
+    let a =50;
+    console.log("inside if ", a); 
+}
+
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);

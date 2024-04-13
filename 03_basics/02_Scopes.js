06f408820028b4f32e7a901e7c89f2bccfb44feb
sankarshan07 <sankarshandesai3107@gmail.com>
2024-04-13 22:55:47 +0530

    mid scope

--- 03_basics/02_Scopes.js
+++ 03_basics/02_Scopes.js
@@ -1,6 +1,6 @@
 // let var const 
 let a = 10 // local scope
-const b = 20
+const b = 20  //clouser  scope, block scope
 var  c = 30  // global scope -through out the programm
 
 if (true){
@@ -9,10 +9,10 @@ if (true){
 }
 
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
-}
+// }
 
 
 
@@ -21,3 +21,40 @@ console.log(a);
 
 // console.log(b);
 // console.log(c);
+
+//nested scope  /// clouser 
+ function one() {
+    const username = "sanky"
+    function two(){
+      const  website =  "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+ }
+
+ one()
+
+
+
+if (true) {
+    const userna ="sanky"
+    if (userna == "sanky") {
+        const website =" facebook"
+        console.log(`Hello ${userna}, your website is ${website}`);
+    }
+}
+
+//*********IMPORTANT******* */
+function addOne(num) {
+    return num +1
+}
+
+console.log(addOne(5));
+
+const addTwo =function (num) {
+    return num + 2
+}
+console.log(addTwo(7));
+
+console.log(typeof addTwo);

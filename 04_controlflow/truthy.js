300c735a0347daf38789e4fee5bc583cbd54ca70
sankarshan <sankarshandesai3107@gmail.com>
2024-04-17 23:06:41 +0530

    truthy/falsy

---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,15 @@
+const userEmail =[]    //empty arry gives true  bt  empyt array doesnt give truthy values
+
+if (userEmail) {
+    console.log("got user email");
+}
+else{
+    console.log("dont have email");
+}
+
+
+// falsy values 
+// false , 0 ,null ,-0, BigInt 0n , undefined , "" , NaN
+
+// truthy values
+//"0","false"  ,"\t", "\n",  \r, 

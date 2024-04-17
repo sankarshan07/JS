6b4b736e058110a01f5e591f14b246df0623e713
sankarshan <sankarshandesai3107@gmail.com>
2024-04-17 23:33:30 +0530

    finish control flow

--- 04_controlflow/truthy.js
+++ 04_controlflow/truthy.js
@@ -12,4 +12,27 @@ else{
 // false , 0 ,null ,-0, BigInt 0n , undefined , "" , NaN
 
 // truthy values
-//"0","false"  ,"\t", "\n",  \r, 
+//"0","false"  ,"\t", "\n",  \r, " ",[],{},funtion() {}
+
+
+if (userEmail.length === 0) {
+    console.log("Array is empty");
+}
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {   //keys() takes parameter of object
+    console.log("Object is empty");
+}
+
+// nuslish coalescing opertor (??):null undefined
+ let val1;
+ val1 =5 ?? 10  //5
+ val1 =null ?? 10   //10
+ val1 = undefined?? 15
+
+
+// terinary operator 
+// condition ? true : false
+const percentage =9
+percentage >=60 ? console.log("pass") : console.log("fail");

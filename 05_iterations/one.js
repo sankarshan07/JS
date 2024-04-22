1bb2011141ae2a6ce8d7173cbb4f0cd6a5ced2de
sankarshan <sankarshandesai3107@gmail.com>
2024-04-22 22:56:41 +0530

    iterations

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,9 +1,22 @@
 //for 
 
-for (let index = 0; index < 10; index++) {
-    const element = index;
+for (let i = 0; i < 10; i++) {
+    const element = i;   //block scope  variable, only accessible within the block
     console.log(element);
+    if (element==5) {
+        console.log("5 is the best number");
+    }
     
 }
 
- 
+console.log("nested loop");
+
+ for (let i = 0; i < 10; i++) {
+console.log(`outer loop value ${i}`);
+for (let j = 0; j < 10; j++) {
+    // console.log(`inner loop value ${j}`);
+    console.log(i+ "*"+ j + "="+(i*j));
+    
+    
+}    
+ }

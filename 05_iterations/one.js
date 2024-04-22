e8ee7ae4cd407fab9a77907c152b944d2956bd2f
sankarshan <sankarshandesai3107@gmail.com>
2024-04-22 21:38:10 +0530

    iterations

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,5 +1,9 @@
 //for 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element);
     
 }
+
+ 

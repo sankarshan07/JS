538b7db15a04ec61aeb36aa1d6c1eaaf44a87745
sankarshan <sankarshandesai3107@gmail.com>
2024-05-09 19:24:57 +0530

    iterations whilr

---
+++ 05_iterations/two.js
@@ -0,0 +1,12 @@
+let index = 0
+while (index <= 10 ) {
+    console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ['flash','superman','batman']
+let i =0
+while (i < myArray.length) {
+    console.log(`Element at index ${i} is ${myArray[i]}`)
+ i = i + 1   
+}
